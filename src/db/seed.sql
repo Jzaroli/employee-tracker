929ab7d587a8d17100daa4aeedee056027c3aaca
Jzaroli <jzaroli@gmail.com>
2024-11-14 11:47:58 -0700

    added async functions to dynamically populate lists in add-functions

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -24,11 +24,11 @@ INSERT INTO roles (title, salary, dept_id) VALUES
 
 -- Inserts seed data into employees table
 INSERT INTO employees (first_name, last_name, role_id, manager) VALUES
-('John', 'Smith', 1, NULL),
-('Jane', 'Dylan', 2, NULL),
+('John', 'Smith', 1, 'None'),
+('Jane', 'Dylan', 2, 'None'),
 ('Lebron', 'James', 3, 'Jane Dylan'),
-('Nancy', 'Jordan', 4, NULL),
+('Nancy', 'Jordan', 4, 'None'),
 ('Elena', 'Wattles', 5, 'Bobby Thorogood'),
-('Bobby', 'Thorogood', 6, NULL),
-('Maya', 'Ceprnic', 7, NULL);
+('Bobby', 'Thorogood', 6, 'None'),
+('Maya', 'Ceprnic', 7, 'None');
 
