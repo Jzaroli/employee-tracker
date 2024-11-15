509cd8ebc4ebf86e0c037399e46c59015c24630b
Jzaroli <jzaroli@gmail.com>
2024-11-14 19:31:59 -0700

    finished update employee functionality, updated queries and added comments

--- dist/index.js
+++ dist/index.js
@@ -16,9 +16,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const inquirer_1 = __importDefault(require("inquirer"));
 const index_js_1 = __importDefault(require("./db/index.js"));
 const cli_table3_1 = __importDefault(require("cli-table3"));
+// Creates Db instance:
 const db = new index_js_1.default();
+// Runs main script when booting up the app:
 script();
-// Function for all of the prompts that get call in the app:
+// Function with the prompts that make up the app:
 function script() {
     inquirer_1.default
         .prompt([
@@ -36,7 +38,7 @@ function script() {
                     value: 'VIEW_ROLES',
                 },
                 {
-                    name: 'View all Employess',
+                    name: 'View all Employees',
                     value: 'VIEW_EMPLOYEES',
                 },
                 {
@@ -52,6 +54,10 @@ function script() {
                     value: 'ADD_EMPLOYEE',
                 },
                 {
+                    name: 'Update an Employee',
+                    value: 'UPDATE_EMPLOYEE'
+                },
+                {
                     name: 'Quit Application',
                     value: 'QUIT_APPLICATION',
                 }
@@ -60,7 +66,7 @@ function script() {
     ])
         .then((response) => {
         const choice = response.choice;
-        //Calls the appropriate function bacsed on the selected prompt in script
+        //Calls the appropriate function based on the selected prompt in the script:
         switch (choice) {
             case 'VIEW_DEPARTMENTS':
                 findDepartments();
@@ -80,6 +86,9 @@ function script() {
             case 'ADD_EMPLOYEE':
                 addEmployee();
                 break;
+            case 'UPDATE_EMPLOYEE':
+                updateEmployee();
+                break;
             case 'QUIT_APPLICATION':
                 quit();
                 break;
@@ -88,6 +97,7 @@ function script() {
         }
     });
 }
+// Function that connects script() to the queries in db/index.js for viewing departments
 function findDepartments() {
     db.viewAllDepartments()
         .then(({ rows }) => {
@@ -101,6 +111,7 @@ function findDepartments() {
     })
         .then(() => script());
 }
+// Function that connects script() to the queries in db/index.js for adding departments
 function addDepartment() {
     inquirer_1.default.prompt([
         {
@@ -116,6 +127,7 @@ function addDepartment() {
     })
         .then(() => script());
 }
+// Function that connects script() to the queries in db/index.js for viewing roles:
 function findRoles() {
     db.viewAllRoles()
         .then(({ rows }) => {
@@ -129,12 +141,14 @@ function findRoles() {
     })
         .then(() => script());
 }
+// Async function that returns departments to populate in addRole():
 function returnDeptsId() {
     return __awaiter(this, void 0, void 0, function* () {
         const departments = yield db.returnDept();
         return departments.rows.map(row => row.dept_id);
     });
 }
+// Function that connects script() to the queries in db/index.js for adding roles:
 function addRole() {
     return __awaiter(this, void 0, void 0, function* () {
         const departments = yield returnDeptsId();
@@ -177,23 +191,25 @@ function findEmployees() {
     })
         .then(() => script());
 }
+// Async function that returns roles to populate in addEmployee():
 function returnRolesId() {
     return __awaiter(this, void 0, void 0, function* () {
         const roles = yield db.returnRole();
         return roles.rows.map(role => role.role_id);
     });
 }
+// Async function that returns managers to populate in addEmployee():
 function returnManagers() {
     return __awaiter(this, void 0, void 0, function* () {
         const managers = yield db.returnManager();
         return managers.rows.map(manager => manager.manager);
     });
 }
+// Function that connects script() to the queries in db/index.js for adding employees:
 function addEmployee() {
     return __awaiter(this, void 0, void 0, function* () {
         const roles = yield returnRolesId();
         const managers = yield returnManagers();
-        const none = 'None';
         inquirer_1.default.prompt([
             {
                 type: "input",
@@ -215,7 +231,7 @@ function addEmployee() {
                 type: "list",
                 name: "manager",
                 message: "If applicable, who is the manager for this new employee?",
-                choices: [...managers, none]
+                choices: [...managers, 'None']
             }
         ]).then((response) => {
             db.addEmp(response);
@@ -226,6 +242,86 @@ function addEmployee() {
             .then(() => script());
     });
 }
+// Async function that returns the employees and their IDs as a string for updateEmployee():
+function returnExistingEmpsName() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const employees = yield db.returnExistEmps();
+        return employees.rows.map(employee => `${employee.employee_id} ${employee.first_name} ${employee.last_name}`);
+    });
+}
+// Function that connects script() to the queries in db/index.js for updating employees:
+function updateEmployee() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Stores employee information for initial employee selection:
+        const employees = yield returnExistingEmpsName();
+        // Select the employee you want to update:
+        inquirer_1.default.prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "Which employee would you like to update?",
+                choices: employees
+            }
+        ]).then((employee) => {
+            // Passes the id down the line so the function knows which record to update in the db:
+            const employeeSplit = employee.employee.split('');
+            const id = employeeSplit[0];
+            return id;
+        })
+            .then((id) => secondPrompt(id));
+        // This function gathers the details for the updated employee record:
+        function secondPrompt(id) {
+            return __awaiter(this, void 0, void 0, function* () {
+                const roles = yield returnRolesId();
+                const managers = yield returnManagers();
+                inquirer_1.default.prompt([
+                    {
+                        type: "input",
+                        name: "newFirstName",
+                        message: "What is this employee's first name?",
+                    },
+                    {
+                        type: "input",
+                        name: "newLastName",
+                        message: "What is this employee's last name?",
+                    },
+                    {
+                        type: "list",
+                        name: "newRole",
+                        message: "What is this employee's role ID?",
+                        choices: roles
+                    },
+                    {
+                        type: "list",
+                        name: "newManager",
+                        message: "If applicable, who is the manager for this new employee?",
+                        choices: [...managers, 'None']
+                    }
+                ]).then((response) => {
+                    // The ID needed the be converted to a number type:
+                    const newId = Number(id);
+                    // Creates an empty ID object and pushes the ID number to it:
+                    const empId = {};
+                    empId.empId = newId;
+                    // Creates a final object to be passed to the SQL query combining the initial employee ID with the udpated employee information:
+                    const finalObj = {};
+                    finalObj.finalId = empId.empId;
+                    finalObj.finalFName = response.newFirstName;
+                    finalObj.finalLName = response.newLastName;
+                    finalObj.finalRole = response.newRole;
+                    finalObj.finalManager = response.newManager;
+                    // Sends the object to the query for the update:
+                    db.updateEmp(finalObj);
+                })
+                    .then(() => {
+                    console.log('Your employee was updated in the system record!');
+                })
+                    .then(() => script());
+            });
+        }
+    });
+}
+// Function for quitting app in switch of script():
 function quit() {
     console.log('Exiting Application');
     process.exit(0);
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { pool } from './connection.js';
 
+// Create class DB, async query function and associated methods for manipulating data:
 export default class Db {
     constructor() {}
 
@@ -13,30 +14,35 @@ export default class Db {
         }
     }
 
+// Selects all from departments:
     async viewAllDepartments() {
         return this.query(
-            'SELECT * FROM departments;'
+            'SELECT * FROM departments ORDER BY dept_id ASC;'
         );
     }
 
+// Selects dept for add role function:
     async returnDept() {
         return this.query(
-            'SELECT dept_id FROM roles;'
+            'SELECT dept_id FROM departments;'
         );
     }
-
+    
+// Selects important infromation for employees view:
     async viewAllEmployees() {
         return this.query(
-            'SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title, roles.salary, employees.manager, departments.dept_name FROM employees JOIN roles ON employees.role_id = roles.role_id JOIN departments ON roles.dept_id = departments.dept_id;'
+            'SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title, roles.salary, employees.manager, departments.dept_name FROM employees JOIN roles ON employees.role_id = roles.role_id JOIN departments ON roles.dept_id = departments.dept_id ORDER BY employee_id ASC;'
         );
     }
 
+// Selects important role information for roles view:
     async viewAllRoles() {
         return this.query(
-            'SELECT roles.role_id, roles.title, roles.salary, departments.dept_name FROM roles JOIN departments ON roles.dept_id = departments.dept_id;'
+            'SELECT roles.role_id, roles.title, roles.salary, departments.dept_name FROM roles JOIN departments ON roles.dept_id = departments.dept_id ORDER BY role_id ASC;'
         );
     }
 
+// Inserts new department information into departments table:
     async addDept (department: any) {
         const deptName = department.name;
         return this.query (
@@ -45,6 +51,7 @@ export default class Db {
         );
     }
 
+// Inserts new department information into departments table:
     async addRole (role: any) {
         return this.query (
             'INSERT INTO roles (title, salary, dept_id) VALUES ($1, $2, $3)',
@@ -52,6 +59,7 @@ export default class Db {
         );
     }
 
+// Inserts new employee information into employees table:
     async addEmp (employee: any) {
         return this.query (
             'INSERT INTO employees (first_name, last_name, role_id, manager) VALUES ($1, $2, $3, $4)',
@@ -59,15 +67,36 @@ export default class Db {
         );
     }
 
+// Returns manager for add employee function:
     async returnManager() {
         return this.query(
-            'SELECT manager FROM employees WHERE manager IS NOT NULL'
+            `SELECT manager FROM employees WHERE manager <> $1`, 
+            ['None']
         );
     }
 
+// Returns role for add employee function:
     async returnRole() {
         return this.query(
-            'SELECT role_id FROM employees'
+            'SELECT role_id FROM roles'
+        );
+    }
+
+// Returns existing employees for udpate employee function:
+    async returnExistEmps() {
+        return this.query(
+            'SELECT first_name, last_name, employee_id FROM employees'
+        );
+    }
+
+// Updates existing employee in employees table:
+    async updateEmp(employeeInfo: any) {
+        return this.query(
+            'UPDATE employees SET first_name = $2, last_name = $3, role_id = $4, manager = $5 WHERE employee_id = $1',
+            [employeeInfo.finalId, employeeInfo.finalFName, employeeInfo.finalLName, employeeInfo.finalRole, employeeInfo.finalManager]    
         );
     }
 }
+
+
+
